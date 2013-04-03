6920891d08928b6026cb204d19563a9d1d26e14a
Lauri Pullinen <lauri.pullinen@aalto.fi>
2013-04-03 11:07:50 +0300

    kentät päivitetty

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,5 +1,5 @@
 class Beer < ActiveRecord::Base
-	attr_accessible :name, :beer_type, :alc
+	attr_accessible :name, :beer_type, :alc, :brewery, :country, :img
 
 	validates :name, :beer_type, :alc, presence: true
 end
