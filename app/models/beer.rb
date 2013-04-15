ce97c9734ad33a89448c4d9500265ea6169786f8
Lauri Pullinen <lauri.pullinen@aalto.fi>
2013-04-15 12:54:54 +0300

    searching is now case insensitive

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -5,7 +5,8 @@ class Beer < ActiveRecord::Base
 
 	def self.search(search_cond)
 		if search_cond
-			find(:all, :conditions => ['name LIKE :s OR beer_type LIKE :s OR brewery LIKE :s OR country LIKE :s', :s => "%#{search_cond}%"])
+			search_cond_lower = search_cond.downcase
+			find(:all, :conditions => ['lower(name) LIKE :s OR lower(beer_type) LIKE :s OR lower(brewery) LIKE :s OR lower(country) LIKE :s', :s => "%#{search_cond_lower}%"])
 		else
 			find(:all)
 		end
