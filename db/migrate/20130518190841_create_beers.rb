d360ae455cd209ad00b7416a8e7370f7b6ddfc86
Lauri Pullinen <lauri.pullinen@aalto.fi>
2013-05-18 22:14:37 +0300

    still migrations...

---
+++ db/migrate/20130518190841_create_beers.rb
@@ -0,0 +1,14 @@
+class CreateBeers < ActiveRecord::Migration
+  def change
+  	create_table :beers do |t|
+      t.string :name
+      t.string :beer_type
+      t.integer :alc
+      t.string :brewery
+      t.string :country
+      t.string :img
+
+      t.timestamps
+    end
+  end
+end
