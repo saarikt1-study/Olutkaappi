193f1fb45f826e8e67bb77381b45bbdfa0d175e1
Lauri Pullinen <lauri.pullinen@aalto.fi>
2013-05-23 22:46:22 +0300

    checkbox validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-	attr_accessible :username, :password, :email
+	attr_accessible :username, :password, :email, :terms
 	validates :email, uniqueness: true
 	validates :email, :password, presence: true
+	validates_acceptance_of :terms
     include Gravtastic
     gravtastic :secure => true,
               :filetype => :gif,
